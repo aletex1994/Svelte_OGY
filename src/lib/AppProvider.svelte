5e4b481272178834e2658b76640cf0cc4b15598f
AleTex <68469149+aletex1994@users.noreply.github.com>
2022-09-30 16:14:08 +0200

    Structure

---
+++ src/lib/AppProvider.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import SnackBar from './SnackBar.svelte'
+</script>
+<slot/>
+<SnackBar message="Hello World"/>
+
---
+++ src/lib/SnackBar.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+
+export let message: string;
+</script>
+<p>{message}</p>
+
